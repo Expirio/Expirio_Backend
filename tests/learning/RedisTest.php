503fdcdde69a5d4d436ffc22357388b7c252480b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-21 22:05:39 +0000

    basic operations for redis done

--- tests/learning/RedisTest.php
+++ tests/learning/RedisTest.php
@@ -35,12 +35,45 @@ class RedisTest extends TestCase
 		$hash = ['country' => 'Africa', 'age' => 12];
 
 		$this->redis->hmset('person', $hash);
+		$hashRead = $this->redis->hgetall('person');
 
-		$this->assertSame(
-			$hash = ['country' => 'Africa', 'age' => '12'],
-			$this->redis->hgetall('person'),
+		$this->assertSame(['country' => 'Africa', 'age' => '12'], $hashRead,
 			'Numbers are not converted to int when reading'
 		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function can_add_field_to_hash()
+	{
+		$hash = ['country' => 'Africa', 'age' => 12];
+
+		$this->redis->hmset('person', $hash);
+		$this->redis->hset('person', 'color', 'black');
+		$hashRead = $this->redis->hgetall('person');
+
+		$this->assertSame(['country' => 'Africa', 'age' => '12', 'color' => 'black'], $hashRead);
+	}
+
+	/**
+	 * @test
+	 */
+	public function can_remove_field_from_hash()
+	{
+		$hash = ['country' => 'Africa', 'age' => 12];
+
+		$this->redis->hmset('person', $hash);
+		$this->redis->hdel('person', ['age']);
+		$hashRead = $this->redis->hgetall('person');
+
+		$this->assertSame(['country' => 'Africa'], $hashRead);
+	}
+
+	public function tearDown()
+	{
+		$this->redis->flushall();
+		$this->redis->flushdb();
 	}
 
 
