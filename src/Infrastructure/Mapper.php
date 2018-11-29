2fe8271a6932c972aa3592e9b0615b62be92bf33
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-11-29 17:05:55 +0000

    added mapper to domain tests

---
+++ src/Infrastructure/Mapper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Infrastructure;
+
+use App\Domain\ReadSlot\ReadSlot;
+use App\Domain\WriteSlot\WriteSlot;
+
+class Mapper
+{
+	public static function toDomain(string $guid, array $persistenceData)
+	{
+		if (isset($persistenceData['password'])) {
+			$secret = !empty($persistenceData['secret']) ? $persistenceData['secret'] :  null;
+			return new ReadSlot($guid, $persistenceData['password'], $secret, $persistenceData['attempts']);
+		}
+
+		if (isset($persistenceData['read_slot'])) {
+			$secret = !empty($persistenceData['secret']) ? $persistenceData['secret'] :  null;
+			return new WriteSlot($guid, $persistenceData['read_slot'], $secret);
+		}
+	}
+}
---
+++ tests/src/Infrastructure/MapperTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\src\Infrastructure;
+
+use App\Domain\ReadSlot\ReadSlot;
+use App\Domain\WriteSlot\WriteSlot;
+use App\Infrastructure\Mapper;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group infrastructure
+ */
+class MapperTest extends TestCase
+{
+	/**
+	 * @test
+	 */
+	public function read_to_domain()
+	{
+		$givenDataForReadGuid = [
+			'password' => 'mypassword',
+			'attempts' => 2,
+			'secret' => null
+		];
+
+		$readSlot = Mapper::toDomain('guid1', $givenDataForReadGuid);
+		
+		$this->assertInstanceOf(ReadSlot::class, $readSlot);
+	}
+
+	/**
+	 * @test
+	 */
+	public function write_to_domain()
+	{
+		$givenDataForReadGuid = ['read_slot' => 'guid2'];
+
+		$writeSlot = Mapper::toDomain('guid1', $givenDataForReadGuid);
+
+		$this->assertInstanceOf(WriteSlot::class, $writeSlot);
+	}
+}
